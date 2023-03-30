48eefaf933081eb6fd6739788f63b51ac9fd5a15
khushal <khushalvadher229@gmail.com>
2023-03-30 22:14:56 +0530

    send mail to user working

--- server/controller/Mail.js
+++ server/controller/Mail.js
@@ -1,88 +1,41 @@
-const nodemailer = require('nodemailer');
+const nodemailer  = require('nodemailer');
 
-const mail =  (req, res) => {
+//admin can send mail to any user's using call this function and pass the email address of the user
+function Mail(receiver) {
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.MAIL,
+            pass: process.env.MAILPASS
+        },
+    },
+    );
+    var mailOptions = {
+        from: {
+            name: 'Khushal',
+            email: process.env.MAIL
+        },
+        to: receiver,
+        subject: 'Registration Successful at Tournion!',
+        text: 'Congratulation!! Registration Successful',
+        html: `<p>Your Reoport :</p>`,
+        attachments : [
+          {
+            filename:'report.pdf',
+          path:'https://mysy.guj.nic.in/Noticeboard/FAQs.pdf'        }
+         ]
+        
+    };
 
-    try {
-        // 
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'khushalvadher229@gmail.com',
-                pass: process.env.MAILPASS
-            }
-        });
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent to : ' + receiver);
+        }
+    });
+};
 
-        // Define the email options
-        let mailOptions = {
-            from: 'khushalvadher229@gmail.com',
-            to: process.env.MAIL,
-            subject: 'Hello World!',
-            text: 'This is a test email sent from Node.js using Nodemailer.'
-        };
-
-        // Send the email
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-    }catch(err){
-        console.log(err);
-    }
-
-}
-
-
-module.exports = mail;
-// const mail = async (req, res) => {
-//     const transporter = nodemailer.createTransport({
-//         service: "hotmail",
-//         auth: {
-//             user: "khushalvadher229@gmail.com",
-//             pass: process.env.MAILPASS,
-//         },
-//     });
-//     const subject = "app name";
-//     const textbody = `Dear {}`;
-//     const options = {
-//         from: "khushalvadher229@gmail.com",
-//         to: "khushalvadher229@outlook.com",
-//         subject: {},
-//         text: `It's absoulety working!!!`,
-//     };
-
-//     transporter.sendMail(options, function (err, info) {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         console.log("Sent: " + info.response);
-//     });
-// }
-
-
-
-// const sender = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: "khushalvadher229@gmail.com",
-//         pass: "lrkiywqfexawnaje",
-//     }
-// });
-
-// const mail = {
-//     from: "khushalvadher229@gmail.com",
-//     to: "kdvadher1510@gmail.com",
-//     subject: "Sending Email using Node.js",
-//     text: "That was easy!"
-// };
-
-// sender.sendMail(mail).then(()=>{
-//     return res.status(201).json({
-//         mes :"Hey"
-//     })
-// });
-
-// module.exports = mail;
+module.exports = Mail;
